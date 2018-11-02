2ed58de9ecbf7bc14b4a4cb9c6edce48a0547efe
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-02 16:08:54 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,44 @@
+
 class SeasController < ApplicationController
   #define your controller actions here
+
+  def index
+    @seas = Sea.all
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def welcome
+
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.find[:id]
+    @sea.update(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+    redirect_to "/seas"
+  end
 
 
   private
@@ -9,5 +48,6 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
+
 
 end
